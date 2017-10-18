51bed60b9ba40f6746073e9c1eb5536259e11952
ToluwaniO <ogunsanyatoluwani@gmail.com>
2017-10-18 11:05:50 -0400

    Initial commit

---
+++ src/LRUCache.java
@@ -0,0 +1,69 @@
+import java.util.Map;
+
+public class LRUCache<K, V> {
+    public LRUCache(int maxSize){
+
+    }
+
+    public final int createCount(){
+        return 0;
+    }
+
+    public final void evictAll(){
+        return;
+    }
+
+    public final int evictionCount(){
+        return 0;
+    }
+
+    public final V get(){
+        return null;
+    }
+
+    public final int hitCount(){
+        return 0;
+    }
+
+    public final int maxSize(){
+        return 0;
+    }
+
+    public final int missCount(){
+        return 0;
+    }
+
+    public final V put(K key, V value){
+        return null;
+    }
+
+    public final int putCount(){
+        return 0;
+    }
+
+    public final V remove(K key){
+        return null;
+    }
+
+    public void resize(int maxSize){
+        return;
+    }
+
+    public final int size(){
+        return 0;
+    }
+
+    public final Map<K, V> snapShot(){
+        return null;
+    }
+
+    public final String toString(){
+        return "";
+    }
+
+    public void trimToSize(int maxSize){
+        return;
+    }
+
+
+}
